b7ae647d3dd327fb818037ffd54aa328655ed362
junghyun2006 <jung.hyun.park516@gmail.com>
2020-12-02 17:46:07 -0500

    Start 01_sql

--- BYOAR1/lib/00_attr_accessor_object.rb
+++ BYOAR1/lib/00_attr_accessor_object.rb
@@ -1,5 +1,14 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do 
+        return self.instance_variable_get('@'+name.to_s)
+      end
+      define_method(name.to_s+"=") do |input|
+        self.instance_variable_set("@#{name}", input)
+      end
+    end
+    nil
   end
+
 end
--- BYOAR1/lib/01_sql_object.rb
+++ BYOAR1/lib/01_sql_object.rb
@@ -4,19 +4,31 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+
   def self.columns
-    # ...
+    @query ||= DBConnection.execute2(<<-SQL)[0].map(&:to_sym)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT(0)
+      SQL
   end
 
   def self.finalize!
+    self.columns.each do |col|
+       my_attr_accessor(col)
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+      instance_variable_set("@#{self.table_name}", table_name)
   end
 
   def self.table_name
-    # ...
+      class_name = self.to_s
+      result = class_name.chars.map {|char| char == char.upcase ? '_' + char.downcase : char}.join('') + 's'
+      result[1..-1]
   end
 
   def self.all
@@ -32,11 +44,11 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+      
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
@@ -54,4 +66,18 @@ class SQLObject
   def save
     # ...
   end
+
+  def self.my_attr_accessor(*names)
+    names.each do |name|
+      define_method(name) do 
+        @attributes[name.to_sym]
+      end 
+    
+      define_method(name.to_s+"=") do |input = nil|
+        @attributes[name.to_sym] = input
+      end
+    end
+    nil
+  end
+
 end
